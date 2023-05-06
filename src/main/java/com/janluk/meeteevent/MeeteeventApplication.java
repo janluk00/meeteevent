4a395cb11e6da0581e9c384eaeb2346e4c7e7bf6
janluk <jan.lukaszewicz00@wp.pl>
2023-05-06 08:59:19 +0200

    initial commit

---
+++ src/main/java/com/janluk/meeteevent/MeeteeventApplication.java
@@ -0,0 +1,13 @@
+package com.janluk.meeteevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeeteeventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeeteeventApplication.class, args);
+	}
+
+}
