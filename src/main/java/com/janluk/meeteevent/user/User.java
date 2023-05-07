f93f3142e641c8089462f3cd1e57b03f192b9506
janluk <jan.lukaszewicz00@wp.pl>
2023-05-07 19:27:22 +0200

    added user repo and model

---
+++ src/main/java/com/janluk/meeteevent/user/User.java
@@ -0,0 +1,35 @@
+package com.janluk.meeteevent.user;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "city")
+    private String city;
+
+}
