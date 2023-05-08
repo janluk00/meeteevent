72ccb1943b0fe153008b82ce76b7cee6c9a3e142
janluk <jan.lukaszewicz00@wp.pl>
2023-05-08 21:22:10 +0200

    added spring security but temporally exclude it

--- src/main/java/com/janluk/meeteevent/config/Config.java
+++ src/main/java/com/janluk/meeteevent/config/Config.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class MapperConfig {
+public class Config {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
