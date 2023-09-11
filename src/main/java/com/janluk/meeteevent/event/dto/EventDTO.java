0e95176838056a548bdc8cf2d1e9372cef79cd30
janluk00 <mrjankosz1@gmail.com>
2023-09-11 16:53:55 +0200

    EventDto has changed

--- src/main/java/com/janluk/meeteevent/event/dto/EventDTO.java
+++ src/main/java/com/janluk/meeteevent/event/dto/EventDTO.java
@@ -4,11 +4,12 @@ import com.janluk.meeteevent.user.dto.UserDTO;
 
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 public record EventDTO(
+
+        UUID id,
         String name,
         Date date,
-        String description,
-        List<UserDTO> users,
-        UserDTO createdBy
+        String description
 ) {}
