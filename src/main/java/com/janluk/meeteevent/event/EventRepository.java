b3e03d51b770e927311f0c7ae0a974927976b8da
janluk00 <mrjankosz1@gmail.com>
2023-09-11 17:28:34 +0200

    Ep which retrieves all events for given user

--- src/main/java/com/janluk/meeteevent/event/EventRepository.java
+++ src/main/java/com/janluk/meeteevent/event/EventRepository.java
@@ -1,10 +1,19 @@
 package com.janluk.meeteevent.event;
 
 import com.janluk.meeteevent.utils.base.BaseEntityRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface EventRepository extends BaseEntityRepository<Event, UUID> {
+
+    @Query(
+            value = "select event.* from event join user_event on event.id = user_event.event_id " +
+                    "where user_event.user_id = ?1",
+            nativeQuery = true
+    )
+    List<Event> findAllEventsByUserId(UUID userId);
 }
