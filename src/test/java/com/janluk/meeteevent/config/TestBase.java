246c6b45dfe2fe36bc8db1133bfecad1eb75c5cf
janluk00 <mrjankosz1@gmail.com>
2023-11-24 21:55:49 +0100

    update Spring Boot version from 3.0.6 to 3.1.6 and add new dependencies for testing

---
+++ src/test/java/com/janluk/meeteevent/config/TestBase.java
@@ -0,0 +1,15 @@
+package com.janluk.meeteevent.config;
+
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public class TestBase {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:14.9");
+
+}
