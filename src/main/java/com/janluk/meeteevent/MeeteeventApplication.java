bfc6896d2152cc3c58e7a23bb69c227fdbcb65c1
janluk00 <mrjankosz1@gmail.com>
2023-12-16 23:43:36 +0100

    switch to yml config files

--- src/main/java/com/janluk/meeteevent/MeeteeventApplication.java
+++ src/main/java/com/janluk/meeteevent/MeeteeventApplication.java
@@ -5,11 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 
 @EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class MeeteeventApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MeeteeventApplication.class, args);
 	}
